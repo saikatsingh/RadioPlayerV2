e9489a8c8f693d22425cb66013e6d653bb7c05df
saikatsingh <84813706+saikatsingh@users.noreply.github.com>
2021-06-17 02:07:31 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -152,7 +152,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         except:
             pass
     elif query.data=="help":
-        await query.edit_message_text("🙋‍♂️ **Hi Bruh**, \nJust Send Me An Audio File To Play. You Can Use @SafoneMusicBot To Get Audio Files! 😌\n\nCheck /help To Know More ...",
+        await query.edit_message_text("🙋‍♂️ **Hi Bruh**, \nJust Send Me An Audio File To Play. You Can Support my Channel @MoviZenX ! 😌\n\nCheck /help To Know More ...",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
